31dcfe96f9e861e9dbf75ef19953e97ecff09e9d
csanalosibalint <csanalosib@kkszki.hu>
2024-10-08 12:17:41 +0200

    Database export file is added to Solution folder

---
+++ UserApi/Models/UserDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserApi.Models
+{
+    public class UserDbContext : DbContext
+    {
+
+        public UserDbContext() 
+        {
+        
+        }
+        public UserDbContext(DbContextOptions options) : base(options) 
+        {
+        
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string conn = "server=localhost; database=Users; user=root; password=";
+
+                optionsBuilder.UseMySQL(conn);
+            }
+        }
+
+        public DbSet<User> NewUser { get; set; } = null!;
+    }
+}
