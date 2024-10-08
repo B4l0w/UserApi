d0106acdfc0bf3e3c6fa3b725e9438addcf9a956
csanalosibalint <csanalosib@kkszki.hu>
2024-10-08 13:06:37 +0200

    Get endpoint is ready

--- UserApi/obj/Debug/net8.0/UserApi.AssemblyInfo.cs
+++ UserApi/obj/Debug/net8.0/UserApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82f1c356902ef524a45f019fda5aed099a92c589")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31dcfe96f9e861e9dbf75ef19953e97ecff09e9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
