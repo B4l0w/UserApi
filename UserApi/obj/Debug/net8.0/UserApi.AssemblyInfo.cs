fbc67e26070d56e5f9ee6debd6e70b04a49efbce
csanalosibalint <csanalosib@kkszki.hu>
2024-10-08 13:55:32 +0200

    Delete is ready

--- UserApi/obj/Debug/net8.0/UserApi.AssemblyInfo.cs
+++ UserApi/obj/Debug/net8.0/UserApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0106acdfc0bf3e3c6fa3b725e9438addcf9a956")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb1a1cb27e4bfb56537e346a6c0d8415b8d77588")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
